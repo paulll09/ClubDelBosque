bb3ba001e6cf4c8056633c9ddf2b2719f91f3fae
paulll09 <aquinopaul2002@gmail.com>
2025-12-01 21:11:06 -0300

    Usar backend de Hostinger

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // Configuración de la API para conexión local
 //IP: 192.168.0.7
-export const API_URL = "http://192.168.0.13:8080";
+export const API_URL = "https://lightcyan-weasel-722566.hostingersite.com";
